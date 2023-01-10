8c32d487eb04aca368412c6bb0678d1995892656
twinmagix <sazindwandwe1997@gmail.com>
2023-01-10 10:23:54 +0200

    auth commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,71 +1,40 @@
 #!/usr/bin/env python3
-""" Module of auth
+"""
+Manage the API authentication.
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """ Auth Class """
-
-    def __init__(self):
-        """
-            Constructor
-
-            Args:
-                path: path to authenticate
-                excluded_paths: list of excluded path to authenticate
-        """
-
+    """
+    Auth class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-            Require the auth
-
-            Args:
-                path: path to authenticate
-                excluded_paths: list of excluded path to authenticate
-
-            Return:
-                True if is authenticated otherwise false
+        require_auth function
         """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
-
-        if path[-1] is not '/':
-            path += '/'
-
-        for paths in excluded_paths:
-            if paths.endswith('*'):
-                if path.startswith(paths[:-1]):
-                    return False
-            elif path == paths:
+        if path in excluded_paths or path[-1] != '/' and path + '/'\
+                in excluded_paths:
+            return False
+        for p in excluded_paths:
+            if p.endswith('*') and path.startswith(p[:-1]):
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
         """
-            Look the headers
-
-            Args:
-                request: Look the autthorization
-
-            Return:
-                The authorization header or None
+        authorization_header function
         """
-        if request is None:
+        if not request or not request.headers or\
+                not request.headers.get('Authorization'):
             return None
-
-        return request.headers.get('Authorization', None)
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-            Look current user
-
-            Args:
-                request: Look the reques user
-
-            Return:
-                The user
+        current_user function
         """
-        return request
+        return None
